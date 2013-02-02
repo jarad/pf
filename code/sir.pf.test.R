ca1fb25e20e3907fcd934ed9796c4450f54c758f
dsheinson <sheinson@pstat.ucsb.edu>
2013-02-01 16:34:31 -0800

    Removed old functions, now runs pfs on simulated data with 3 unknown parameters

--- code/sir.pf.test.R
+++ code/sir.pf.test.R
@@ -1,178 +1,52 @@
 # Set known parameter values
 P = 5000
 d = 5
-b = c(.25,.27,.23,.29)
-varsigma = c(1.07,1.05,1.01,.98)
-sigma = c(.0012,.0008,.001,.0011)
+b = c(.25, .27, .23, .29)
+varsigma = c(1.07, 1.05, 1.01, .98)
+sigma = 40*c(.0012, .0008, .0010, .0011)
 
 # Set unknown parameter values
-beta = 0.2399
-gamma = 0.1066
-nu = 1.2042
+theta = c(0.2399, 0.1066, 1.2042)
 
-# Functions for simulating data 
-rstate = function(x)
-{
-  tau = 1/d
-  x1var = (x[3] + x[4])*tau^2 / P^2
-  x2var = x[3]*tau^2 / P^2
-  for(i in 1:d)
-  { 
-    is = -1
-    ss = -1
-    while(!(is >= 0 & is <= 1 & ss >= 0 & ss <= 1))
-    {
-      is = x[1] + tau*x[1]*(x[3]*x[2]^x[5] - x[4]) + rnorm(1,0,sqrt(x1var))
-      ss = x[2] - tau*x[3]*x[1]*x[2]^x[5] + rnorm(1,0,sqrt(x2var))
-    }
-    x[1] = is
-    x[2] = ss
-  }
-  return(x)
-}
-robs = function(x)
-{
-  j = length(b)
-  if(!(j == length(varsigma) & j == length(sigma))) stop("b, varsigma, and sigma must all have same length")
-  J = sample(1:j,1)
-  y = rep(NA,j)
-  y[J] = rnorm(1,b[J]*x[1]^varsigma[J],sigma[J])
-  return(y)
-}
-rinit = function() return(c(10/P,1-10/P,beta,gamma,nu))
+# Set prior bounds on unknown parameters
+thetal = c(0.140, 0.090, 0.950)
+thetau = c(0.500, 0.143, 1.300)
 
-# Generate data
+# Simulate epidemic
+source("sir_functions.R")
+revo_sim = function(x){ revo(x, P, d)}
+robs_sim = function(x){ robs(x, b, varsigma, sigma)}
+rinit_sim = function(){ rinit(10/P, theta)}
+nt = 125
 source("ss.sim.R")
-nt = 154
-sim = ss.sim(nt,rstate,robs,rinit)
-
-# Functions to reparameterize theta to and from the real line
-theta2u = function(theta,a,b)
-{
-  etheta = exp(theta)
-  return((b*etheta + a) / (1 + etheta))
-}
-u2theta = function(u,a,b)
-{
-  U = (u - a) / (b - a)
-  return(log(U / (1 - U)))
-}
-
-# Set bounds on unknown parameters
-betal = .14; betau = .5
-gammal = .09; gammau = .143
-nul = .95; nuu = 1.3
-
-# Functions for particle filters
-revo = function(x)
-{
-  tau = 1/d
-  x1var = (x[3] + x[4])*tau^2 / P^2
-  x2var = x[3]*tau^2 / P^2
-  for(i in 1:d)
-  { 
-    is = -1
-    ss = -1
-    while(!(is >= 0 & is <= 1 & ss >= 0 & ss <= 1))
-    {
-      is = x[1] + tau*x[1]*(x[3]*x[2]^x[5] - x[4]) + rnorm(1,0,sqrt(x1var))
-      ss = x[2] - tau*x[3]*x[1]*x[2]^x[5] + rnorm(1,0,sqrt(x2var))
-    }
-    x[1] = is
-    x[2] = ss
-  }
-  return(x)
-}
-revo_kd = function(x,theta=NULL)
-{
-  theta[1] = theta2u(theta[1],betal,betau)
-  theta[2] = theta2u(theta[2],gammal,gammau)
-  theta[3] = theta2u(theta[3],nul,nuu)
-
-  tau = 1/d
-  x1var = (theta[1] + theta[2])*tau^2 / P^2
-  x2var = theta[1]*tau^2 / P^2
-  for(i in 1:d)
-  { 
-    is = -1
-    ss = -1
-    while(!(is >= 0 & is <= 1 & ss >= 0 & ss <= 1))
-    {
-      is = x[1] + tau*x[1]*(theta[1]*x[2]^theta[3] - theta[2]) + rnorm(1,0,sqrt(x1var))
-      ss = x[2] - tau*theta[1]*x[1]*x[2]^theta[3] + rnorm(1,0,sqrt(x2var))
-    }
-    x[1] = is
-    x[2] = ss
-  }
-  return(x)
-}
-dllik = function(y,x,theta=NULL)
-{
-  J = which(!is.na(y))
-  if (!(length(J) == 1)) stop("y must be a vector with all but 1 element NA")
-  h = b[J]*x[1]^varsigma[J]
-  return(dnorm(y[J],h,sigma[J],log=T))
-}
-rprior = function(stateonly=TRUE)
-{
-  require(msm)
-  J = which(!is.na(sim$y[,1]))
-  if (!(length(J) == 1)) stop("y must be a vector with all but 1 element NA")
-  
-  i0 = rtnorm(1,sim$y[J,1]/b[J],sigma[J],0,1)
-  s0 = rtnorm(1,1-sim$y[J,1]/b[J],sigma[J],0,1)
-  beta0 = runif(1,betal,betau)
-  gamma0 = runif(1,gammal,gammau)
-  nu0 = runif(1,nul,nuu)
-  if(stateonly){
-    return(c(i0,s0,beta0,gamma0,nu0))
-  }else{
-    return(list(x=c(i0,s0),theta=u2theta(c(beta0,gamma0,nu0),c(betal,gammal,nul),c(betau,gammau,nuu))))
-  }
-}
-pstate = function(x)
-{
-  tau = 1/d
-  for(i in 1:d)
-  { 
-    x[1] = x[1] + tau*x[1]*(x[3]*x[2]^x[5] - x[4])
-    x[2] = x[2] - tau*x[3]*x[1]*x[2]^x[5]
-  }
-  return(x)
-}
-pstate_kd = function(x,theta=NULL)
-{
-  theta[1] = theta2u(theta[1],betal,betau)
-  theta[2] = theta2u(theta[2],gammal,gammau)
-  theta[3] = theta2u(theta[3],nul,nuu)
-
-  tau = 1/d
-  for(i in 1:d)
-  { 
-    x[1] = x[1] + tau*x[1]*(theta[1]*x[2]^theta[3] - theta[2])
-    x[2] = x[2] - tau*theta[1]*x[1]*x[2]^theta[3]
-  }
-  return(x)
-}
+sim = ss.sim(nt, revo_sim, robs_sim, rinit_sim)
 
 # Run bootstrap filter
+dllik_bf = function(y, x){ dllik(y, x, b, varsigma, sigma)}
+revo_bf = function(x){ revo(x, P, d)}
+rprior_bf = function(){ rprior(sim, thetal, thetau, b, varsigma, sigma)}
 source("bf.R")
-n=100
-out = bf(sim$y, dllik, revo, rprior, n,
+n = 100
+out = bf(sim$y, dllik_bf, revo_bf, rprior_bf, n,
          method="stratified",nonuniformity="ess",threshold=0.8,log=F)
 
-# Run auxilliary particle filter
+# Run auxiliary particle filter
+dllik_apf = function(y, x){ dllik(y, x, b, varsigma, sigma)}
+pstate_apf = function(x) { revo(x, P, d, random = FALSE)}
+revo_apf = function(x){ revo(x, P, d)}
+rprior_apf = function(){ rprior(sim, thetal, thetau, b, varsigma, sigma)}
 source("apf.R")
-out2 = apf(sim$y, dllik, pstate, revo, rprior, n,
-	   method="stratified",nonuniformity="ess",threshold=0.8,log=F)
+out2 = apf(sim$y, dllik_apf, pstate_apf, revo_apf, rprior_apf, n,
+         method="stratified",nonuniformity="ess",threshold=0.8,log=F)
 
 # Run kernel density particle filter
+dllik_kd = function(y, x, theta=NULL){ dllik(y, x, b, varsigma, sigma)}
+pstate_kd = function(x, theta) { revo(x, P, d, theta, thetal, thetau, FALSE, FALSE)}
+revo_kd = function(x, theta){ revo(x, P, d, theta, thetal, thetau, FALSE)}
+rprior_kd = function(){ rprior(sim, thetal, thetau, b, varsigma, sigma, FALSE)}
 source("kd_pf.R")
-rprior_kd = function() rprior(FALSE)
-out3 = kd_pf(sim$y, dllik, pstate_kd, revo_kd, rprior_kd, n,
- 	   method="stratified",nonuniformity="ess",threshold=0.8,log=F)
-
-#load("../Data/sir.pf.test.rdata")
+out3 = kd_pf(sim$y, dllik_kd, pstate_kd, revo_kd, rprior_kd, n,
+         method="stratified",nonuniformity="ess",threshold=0.8,log=F)
 
 # Comparison
 # Plot % infected
@@ -190,7 +64,7 @@ for(i in 1:tt){
 	kd.li[i] = wtd.quantile(out3$state[1,,i], out3$weight[,i], normwt=T, probs=.025)
 	kd.ui[i] = wtd.quantile(out3$state[1,,i], out3$weight[,i], normwt=T, probs=.975)
 }
-pdf(paste("../Graphs/PF/PF-quant-",n,".pdf",sep=""))
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/PF-quant-",n,".pdf",sep=""))
 plot(sim$x[1,],type="l",ylim=c(0,.28),xlab="Time (days)",ylab="% Population",
 	main="95% Credible Intervals of %Pop Infected")
 lines(bf.i,col=2)
@@ -210,54 +84,52 @@ dev.off()
 require(plotrix)
 cutoff = nt + 1
 msize = labsize = 1.5
-pdf(paste("../Graphs/PF/Hist-BF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Hist-BF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
 par(mfrow=c(3,1))
 weighted.hist(out$state[3,,cutoff],out$weight[,cutoff],
 	xlab=expression(beta),main="Histogram of Contact Rate",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
+mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=theta[1])),side=3)
 weighted.hist(out$state[4,,cutoff],out$weight[,cutoff],
 	xlab=expression(gamma),main="Histogram of Recovery Time",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
+mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=theta[2])),side=3)
 weighted.hist(out$state[5,,cutoff],out$weight[,cutoff],
 	xlab=expression(nu),main="Histogram of Mixing Intensity",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
+mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=theta[3])),side=3)
 dev.off()
 
 # auxillary particle filter
-pdf(paste("../Graphs/PF/Hist-APF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Hist-APF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
 par(mfrow=c(3,1))
 weighted.hist(out2$state[3,,cutoff],out2$weight[,cutoff],
 	xlab=expression(beta),main="Histogram of Contact Rate",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
+mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=theta[1])),side=3)
 weighted.hist(out2$state[4,,cutoff],out2$weight[,cutoff],
 	xlab=expression(gamma),main="Histogram of Recovery Time",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
+mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=theta[2])),side=3)
 weighted.hist(out2$state[5,,cutoff],out2$weight[,cutoff],
 	xlab=expression(nu),main="Histogram of Mixing Intensity",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
+mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=theta[3])),side=3)
 dev.off()
 
 # kernel density particle filter
-pdf(paste("../Graphs/PF/Hist-KD-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Hist-KD-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
 par(mfrow=c(3,1))
-weighted.hist(theta2u(out3$theta[1,,cutoff],betal,betau),out3$weight[,cutoff],
+weighted.hist(theta2u(out3$theta[1,,cutoff],thetal[1],thetau[1]),out3$weight[,cutoff],
 	xlab=expression(beta),main="Histogram of Contact Rate",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
-weighted.hist(theta2u(out3$theta[2,,cutoff],gammal,gammau),out3$weight[,cutoff],
+mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=theta[1])),side=3)
+weighted.hist(theta2u(out3$theta[2,,cutoff],thetal[2],thetau[2]),out3$weight[,cutoff],
 	xlab=expression(gamma),main="Histogram of Recovery Time",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
-weighted.hist(theta2u(out3$theta[3,,cutoff],nul,nuu),out3$weight[,cutoff],
+mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=theta[2])),side=3)
+weighted.hist(theta2u(out3$theta[3,,cutoff],thetal[3],thetau[3]),out3$weight[,cutoff],
 	xlab=expression(nu),main="Histogram of Mixing Intensity",
 	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
+mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=theta[3])),side=3)
 dev.off()
-
-#save.image("../Data/sir.pf.test.rdata")
