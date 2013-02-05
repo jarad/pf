294b079c8dee149cf7831c975e6890144cf412d9
dsheinson <sheinson@pstat.ucsb.edu>
2013-02-04 18:36:39 -0800

    Computes and plots predicted epidemic curves using data generated by sir.pf.test and sir.pf.test-6P

--- code/sir.pf.pred.R
+++ code/sir.pf.pred.R
@@ -1,21 +1,8 @@
-load("../Data/sir.pf.test.rdata") #Use same simulated data
-
-# Run bootstrap filter
-source("bf.R")
-n=1000
-out = bf(sim$y, dllik, revo, rprior, n,
-         method="stratified",nonuniformity="ess",threshold=0.8,log=F)
-
-# Run auxilliary particle filter
-source("apf.R")
-out2 = apf(sim$y, dllik, pstate, revo, rprior, n,
-	   method="stratified",nonuniformity="ess",threshold=0.8,log=F)
-
-# Run kernel density particle filter
-source("kd_pf.R")
-rprior_kd = function() rprior(FALSE)
-out3 = kd_pf(sim$y, dllik, pstate_kd, revo_kd, rprior_kd, n,
- 	   method="stratified",nonuniformity="ess",threshold=0.8,log=F)
+# Load particle filter data
+param = "-6P"
+num = "-10000"
+load(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Data/sir.pf.test",param,num,".rdata",sep=""))
+ns = dim(sim$x)[1]
 
 # Function needed to generate predicted SIR curves
 ss.pred = function(nt,revo,rinit)
@@ -38,131 +25,270 @@ ss.pred = function(nt,revo,rinit)
   return(x)
 }
 
-# Get 95% Credible intervals of predicted epidemic curve after running PFs for different numbers of days
+# Predict epidemic by propagating particles after being filtered for different numbers of days
+require(Hmisc)
 days <- seq(10,35,5)
-bf.li <- bf.ui <- list()
-apf.li <- apf.ui <- list()
-kd.li <- kd.ui <- list()
-length(bf.li) = length(bf.ui) = length(days)
-length(apf.li) = length(apf.ui) = length(days)
-length(kd.li) = length(kd.ui) = length(days)
+bf.li <- bf.i <- bf.ui <- list()
+apf.li <- apf.i <- apf.ui <- list()
+kd.li <- kd.i <- kd.ui <- list()
+bf.peak.prop <- bf.peak.calc <- list()
+apf.peak.prop <- apf.peak.calc <- list()
+kd.peak.prop <- kd.peak.calc <- list()
+length(bf.li) = length(bf.i) = length(bf.ui) = length(days)
+length(apf.li) = length(apf.i) = length(apf.ui) = length(days)
+length(kd.li) = length(kd.i) = length(kd.ui) = length(days)
+length(bf.peak.prop) = length(bf.peak.calc) = length(days)
+length(apf.peak.prop) = length(apf.peak.calc) = length(days)
+length(kd.peak.prop) = length(kd.peak.calc) = length(days)
+pb = txtProgressBar(0,n*length(days),style=3)
 for(j in 1:length(days))
 {
+  # Grab  particles from each filter at time days[j]
   mystate = out$state[,,days[j]+1]
   mystate2 = out2$state[,,days[j]+1]
-  mybeta = theta2u(out3$theta[1,,days[j]+1],betal,betau)
-  mygamma = theta2u(out3$theta[2,,days[j]+1],gammal,gammau)
-  mynu = theta2u(out3$theta[3,,days[j]+1],nul,nuu)
-  mystate3 = rbind(out3$state[,,days[j]+1],mybeta,mygamma,mynu)
-  pred = pred2 = pred3 = matrix(NA,n,nt-days[j])
-  pb = txtProgressBar(0,n,style=3)
+  mybeta = theta2u(out3$theta[1,,days[j]+1],thetal[1],thetau[1])
+  mygamma = theta2u(out3$theta[2,,days[j]+1],thetal[2],thetau[2])
+  mynu = theta2u(out3$theta[3,,days[j]+1],thetal[3],thetau[3])
+  if(ns == 8) {
+    myb = theta2u(out3$theta[4,,days[j]+1],thetal[4],thetau[4])
+    myvarsigma = theta2u(out3$theta[5,,days[j]+1],thetal[5],thetau[5])
+    mysigma = theta2u(out3$theta[6,,days[j]+1],thetal[6],thetau[6])
+    mystate3 = rbind(out3$state[,,days[j]+1],mybeta,mygamma,mynu,myb,myvarsigma,mysigma)
+  } else if(ns == 5) {
+    mystate3 = rbind(out3$state[,,days[j]+1],mybeta,mygamma,mynu)  
+  } else { stop("Wrong number of parameters") }
+
+  # Propagate particles forward to end of epidemic
+  pred = pred2 = pred3 = array(NA,dim=c(8,n,nt-days[j]))
   for(i in 1:n)
   {
-    setTxtProgressBar(pb,i)
-    pred[i,] = ss.pred(nt-(days[j]+1),pstate,function() return(mystate[,i]))[1,]
-    pred2[i,] = ss.pred(nt-(days[j]+1),pstate,function() return(mystate2[,i]))[1,]
-    pred3[i,] = ss.pred(nt-(days[j]+1),pstate,function() return(mystate3[,i]))[1,]
+    setTxtProgressBar(pb,i + n*(j-1))
+    pred[,i,] = ss.pred(nt-(days[j]+1),pstate_apf,function() return(mystate[,i]))
+    pred2[,i,] = ss.pred(nt-(days[j]+1),pstate_apf,function() return(mystate2[,i]))
+    pred3[,i,] = ss.pred(nt-(days[j]+1),pstate_apf,function() return(mystate3[,i]))
   }
-  bf.li[[j]] = bf.ui[[j]] = apf.li[[j]] = apf.ui[[j]] = kd.li[[j]] = kd.ui[[j]] = rep(NA,nt-days[j])
+
+  # Calculate .025 and .975 quantiles of predicted particle samples
+  bf.li[[j]] = bf.i[[j]] = bf.ui[[j]] = apf.li[[j]] = apf.i[[j]] = apf.ui[[j]] = kd.li[[j]] = kd.i[[j]] = kd.ui[[j]] = rep(NA,nt-days[j])
   for(i in 1:(nt-days[j]))
   {
-    bf.li[[j]][i] = wtd.quantile(pred[,i], out$weight[,days[j]+1], normwt=T, probs=.025)
-    bf.ui[[j]][i] = wtd.quantile(pred[,i], out$weight[,days[j]+1], normwt=T, probs=.975)
-    apf.li[[j]][i] = wtd.quantile(pred2[,i], out2$weight[,days[j]+1], normwt=T, probs=.025)
-    apf.ui[[j]][i] = wtd.quantile(pred2[,i], out2$weight[,days[j]+1], normwt=T, probs=.975)
-    kd.li[[j]][i] = wtd.quantile(pred3[,i], out3$weight[,days[j]+1], normwt=T, probs=.025)
-    kd.ui[[j]][i] = wtd.quantile(pred3[,i], out3$weight[,days[j]+1], normwt=T, probs=.975)
+    bf.li[[j]][i] = wtd.quantile(pred[1,,i], out$weight[,days[j]+1], normwt=T, probs=.025)
+    bf.i[[j]][i] = wtd.quantile(pred[1,,i], out$weight[,days[j]+1], normwt=T, probs=.5)
+    bf.ui[[j]][i] = wtd.quantile(pred[1,,i], out$weight[,days[j]+1], normwt=T, probs=.975)
+    apf.li[[j]][i] = wtd.quantile(pred2[1,,i], out2$weight[,days[j]+1], normwt=T, probs=.025)
+    apf.i[[j]][i] = wtd.quantile(pred2[1,,i], out2$weight[,days[j]+1], normwt=T, probs=.5)
+    apf.ui[[j]][i] = wtd.quantile(pred2[1,,i], out2$weight[,days[j]+1], normwt=T, probs=.975)
+    kd.li[[j]][i] = wtd.quantile(pred3[1,,i], out3$weight[,days[j]+1], normwt=T, probs=.025)
+    kd.i[[j]][i] = wtd.quantile(pred3[1,,i], out3$weight[,days[j]+1], normwt=T, probs=.5)
+    kd.ui[[j]][i] = wtd.quantile(pred3[1,,i], out3$weight[,days[j]+1], normwt=T, probs=.975)
   }
-  print(j)
+
+  # Get peak times and intensities of particles
+  # By propagation
+  peak_time = matrix(NA,nr=n,nc=3)
+  peak_intensity = matrix(NA,nr=n,nc=3)
+  get.peak.time = function(x){which(x == max(x))}
+  peak_intensity[,1] = apply(pred[1,,],1,max)
+  peak_intensity[,2] = apply(pred2[1,,],1,max)
+  peak_intensity[,3] = apply(pred3[1,,],1,max)
+  peak_time[,1] = apply(pred[1,,],1,get.peak.time) - 1 + days[j] 
+  peak_time[,2] = apply(pred2[1,,],1,get.peak.time) - 1 + days[j] 
+  peak_time[,3] = apply(pred3[1,,],1,get.peak.time) - 1 + days[j] 
+  # By calculation
+  peak_time_calc = matrix(NA,nr=n,nc=3)
+  peak_intensity_calc = matrix(NA,nr=n,nc=3)
+  calc.peak.time = function(x) log(1/x[ns+1])/(x[4]*(x[3]/x[4] - 1)) + 0.4/x[4]
+  calc.peak.intensity = function(x) 1 - x[4]/x[3] + log(x[4]/x[3])/(x[3]/x[4])
+  peak_intensity_calc[,1] = apply(rbind(mystate,out$state[1,,1]),2,calc.peak.intensity)
+  peak_intensity_calc[,2] = apply(rbind(mystate2,out2$state[1,,1]),2,calc.peak.intensity)
+  peak_intensity_calc[,3] = apply(rbind(mystate3,out3$state[1,,1]),2,calc.peak.intensity)
+  peak_time_calc[,1] = apply(rbind(mystate,out$state[1,,1]),2,calc.peak.time)
+  peak_time_calc[,2] = apply(rbind(mystate2,out2$state[1,,1]),2,calc.peak.time)
+  peak_time_calc[,3] = apply(rbind(mystate3,out3$state[1,,1]),2,calc.peak.time)
+
+  # Calculate .025, .5, and .975 quantiles of peak times and intensities
+  bf.peak.prop[[j]] = apf.peak.prop[[j]] = kd.peak.prop[[j]] = matrix(NA,nr=2,nc=3)
+  bf.peak.prop[[j]][1,] = wtd.quantile(peak_time[,1],out$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  apf.peak.prop[[j]][1,] = wtd.quantile(peak_time[,2],out2$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  kd.peak.prop[[j]][1,] = wtd.quantile(peak_time[,3],out3$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  bf.peak.prop[[j]][2,] = wtd.quantile(peak_intensity[,1],out$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  apf.peak.prop[[j]][2,] = wtd.quantile(peak_intensity[,2],out2$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  kd.peak.prop[[j]][2,] = wtd.quantile(peak_intensity[,3],out3$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  
+  bf.peak.calc[[j]] = apf.peak.calc[[j]] = kd.peak.calc[[j]] = matrix(NA,nr=2,nc=3)
+  bf.peak.calc[[j]][1,] = wtd.quantile(peak_time_calc[,1],out$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  apf.peak.calc[[j]][1,] = wtd.quantile(peak_time_calc[,2],out2$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  kd.peak.calc[[j]][1,] = wtd.quantile(peak_time_calc[,3],out3$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  bf.peak.calc[[j]][2,] = wtd.quantile(peak_intensity_calc[,1],out$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  apf.peak.calc[[j]][2,] = wtd.quantile(peak_intensity_calc[,2],out2$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
+  kd.peak.calc[[j]][2,] = wtd.quantile(peak_intensity_calc[,3],out3$weight[,days[j]+1],normwt=T,probs=c(.025,.5,.975))
 }
 
-# Plot true % population infected
+save.image(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Data/sir.pf.pred",param,"-",n,".rdata",sep=""))
+
+# Plot predicted % population infected
 # Bootstrap filter
-plot(sim$x[1,],type="l",ylim=c(0,.35),xlab="Time (days)",ylab="% Population",
-	main="95% Credible Intervals of Predicted %Pop Infected")
-mtext(paste("n = ",n," particles",sep=""),side=3)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/EpidPred-BF",param,"-",n,".pdf",sep=""),
+	width=10,height=8)
+par(mfrow=c(2,3))
+ymax = max(sapply(bf.ui,max),max(sim$x[1,]))
 for(j in 1:length(days))
 {
-  lines((days[j]+1):nt,bf.li[[j]],lty=2,col=j)
-  lines((days[j]+1):nt,bf.ui[[j]],lty=2,col=j)
+  plot(sim$x[1,],type="l",ylim=c(0,ymax),xlab="Time (days)",ylab="% Population",
+	main="Predicted %Pop Infected")
+  mtext(paste("k = ",days[j]," Days",sep=""),side=3)
+  lines((days[j]+1):nt,bf.i[[j]],col=2)
+  lines((days[j]+1):nt,bf.li[[j]],lty=2,col=2)
+  lines((days[j]+1):nt,bf.ui[[j]],lty=2,col=2)
+  if(j == 1){ legend("topright",legend=c("Truth","Median","95% CI"),
+	lty=c(1,1,2),col=c(1,2,2))}
 }
-legend("topright",legend=c("Truth",days),lty=c(1,rep(2,length(days))),col=c(1,1:length(days)),title="Days")
+dev.off()
 
-# Auxilliary particle filter
-windows()
-plot(sim$x[1,],type="l",ylim=c(0,.35),xlab="Time (days)",ylab="% Population",
-	main="95% Credible Intervals of Predicted %Pop Infected")
-mtext(paste("n = ",n," particles",sep=""),side=3)
+# Auxiliary particle filter
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/EpidPred-APF",param,"-",n,".pdf",sep=""),
+	width=10,height=8)
+par(mfrow=c(2,3))
+ymax = max(sapply(apf.ui,max),max(sim$x[1,]))
 for(j in 1:length(days))
 {
-  lines((days[j]+1):nt,apf.li[[j]],lty=2,col=j)
-  lines((days[j]+1):nt,apf.ui[[j]],lty=2,col=j)
+  plot(sim$x[1,],type="l",ylim=c(0,ymax),xlab="Time (days)",ylab="% Population",
+	main="Predicted %Pop Infected")
+  mtext(paste("k = ",days[j]," Days",sep=""),side=3)
+  lines((days[j]+1):nt,apf.i[[j]],col=4)
+  lines((days[j]+1):nt,apf.li[[j]],lty=2,col=4)
+  lines((days[j]+1):nt,apf.ui[[j]],lty=2,col=4)
+  if(j == 1){ legend("topright",legend=c("Truth","Median","95% CI"),
+	lty=c(1,1,2),col=c(1,4,4))}
 }
-legend("topright",legend=c("Truth",days),lty=c(1,rep(2,length(days))),col=c(1,1:length(days)),title="Days")
+dev.off()
 
 # Kernel density particle filter
-windows()
-plot(sim$x[1,],type="l",ylim=c(0,.35),xlab="Time (days)",ylab="% Population",
-	main="95% Credible Intervals of Predicted %Pop Infected")
-mtext(paste("n = ",n," particles",sep=""),side=3)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/EpidPred-KD",param,"-",n,".pdf",sep=""),
+	width=10,height=8)
+par(mfrow=c(2,3))
+ymax = max(sapply(kd.ui,max),max(sim$x[1,]))
 for(j in 1:length(days))
 {
-  lines((days[j]+1):nt,kd.li[[j]],lty=2,col=j)
-  lines((days[j]+1):nt,kd.ui[[j]],lty=2,col=j)
+  plot(sim$x[1,],type="l",ylim=c(0,ymax),xlab="Time (days)",ylab="% Population",
+	main="Predicted %Pop Infected")
+  mtext(paste("k = ",days[j]," Days",sep=""),side=3)
+  lines((days[j]+1):nt,kd.i[[j]],col=3)
+  lines((days[j]+1):nt,kd.li[[j]],lty=2,col=3)
+  lines((days[j]+1):nt,kd.ui[[j]],lty=2,col=3)
+  if(j == 1){ legend("topright",legend=c("Truth","Median","95% CI"),
+	lty=c(1,1,2),col=c(1,3,3))}
 }
-legend("topright",legend=c("Truth",days),lty=c(1,rep(2,length(days))),col=c(1,1:length(days)),title="Days")
-
-# Histograms of unknown parameters at day 25
-# bootstrap filter
-require(plotrix)
-cutoff = 26
-msize = labsize = 1.5
-pdf(paste("../Graphs/PF/Hist-BF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
-par(mfrow=c(3,1))
-weighted.hist(out$state[3,,cutoff],out$weight[,cutoff],
-	xlab=expression(beta),main="Histogram of Contact Rate",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
-weighted.hist(out$state[4,,cutoff],out$weight[,cutoff],
-	xlab=expression(gamma),main="Histogram of Recovery Time",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
-weighted.hist(out$state[5,,cutoff],out$weight[,cutoff],
-	xlab=expression(nu),main="Histogram of Mixing Intensity",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
 dev.off()
 
-# auxillary particle filter
-pdf(paste("../Graphs/PF/Hist-APF-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
-par(mfrow=c(3,1))
-weighted.hist(out2$state[3,,cutoff],out2$weight[,cutoff],
-	xlab=expression(beta),main="Histogram of Contact Rate",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
-weighted.hist(out2$state[4,,cutoff],out2$weight[,cutoff],
-	xlab=expression(gamma),main="Histogram of Recovery Time",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
-weighted.hist(out2$state[5,,cutoff],out2$weight[,cutoff],
-	xlab=expression(nu),main="Histogram of Mixing Intensity",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
+# Plot predicted peak times and intensities
+tpeak = which(sim$x[1,]==max(sim$x[1,]))-1
+ipeak = max(sim$x[1,])
+
+# Bootstrap filter
+times = sapply(bf.peak.prop,function(x) x[1,2])
+times.l = sapply(bf.peak.prop,function(x) x[1,1])
+times.u = sapply(bf.peak.prop,function(x) x[1,3])
+timesc = sapply(bf.peak.calc,function(x) x[1,2])
+timesc.l = sapply(bf.peak.calc,function(x) x[1,1])
+timesc.u = sapply(bf.peak.calc,function(x) x[1,3])
+ints = sapply(bf.peak.prop,function(x) x[2,2])
+ints.l = sapply(bf.peak.prop,function(x) x[2,1])
+ints.u = sapply(bf.peak.prop,function(x) x[2,3])
+intsc = sapply(bf.peak.calc,function(x) x[2,2])
+intsc.l = sapply(bf.peak.calc,function(x) x[2,1])
+intsc.u = sapply(bf.peak.calc,function(x) x[2,3])
+ymin = min(times.l,timesc.l,tpeak)
+ymax = max(times.u,timesc.u,tpeak)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Peaks-BF",param,"-",n,".pdf",sep=""),width=10,height=5)
+par(mfrow=c(1,2))
+plot(days,times,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Peak (Days)",main="Predicted Time of Epidemic Peak")
+points(days+0.5,timesc,pch=2)
+segments(days,times.l,days,times.u)
+segments(days+.5,timesc.l,days+.5,timesc.u)
+abline(h=tpeak,lty=2)
+legend("top",legend=c("Prop","Calc","95% Cred Int","Truth"),lty=c(NA,NA,1,2),
+	pch=c(1,2,NA,NA))
+ymin = min(ints.l,intsc.l,ipeak)
+ymax = max(ints.u,intsc.u,ipeak)
+plot(days,ints,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Proportion Infected",
+	main="Predicted Intensity of Epidemic Peak")
+points(days+0.5,intsc,pch=2)
+segments(days,ints.l,days,ints.u)
+segments(days+.5,intsc.l,days+.5,intsc.u)
+abline(h=ipeak,lty=2)
 dev.off()
 
-# kernel density particle filter
-pdf(paste("../Graphs/PF/Hist-KD-",n,"-day",cutoff-1,".pdf",sep=""),width=5,height=8)
-par(mfrow=c(3,1))
-weighted.hist(theta2u(out3$theta[1,,cutoff],betal,betau),out3$weight[,cutoff],
-	xlab=expression(beta),main="Histogram of Contact Rate",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(beta," = ",aa,sep=""),list(aa=beta)),side=3)
-weighted.hist(theta2u(out3$theta[2,,cutoff],gammal,gammau),out3$weight[,cutoff],
-	xlab=expression(gamma),main="Histogram of Recovery Time",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(gamma," = ",aa,sep=""),list(aa=gamma)),side=3)
-weighted.hist(theta2u(out3$theta[3,,cutoff],nul,nuu),out3$weight[,cutoff],
-	xlab=expression(nu),main="Histogram of Mixing Intensity",
-	cex.main=msize,cex.lab=labsize)
-mtext(substitute(paste(nu," = ",aa,sep=""),list(aa=nu)),side=3)
+# Auxiliary particle filter
+times = sapply(apf.peak.prop,function(x) x[1,2])
+times.l = sapply(apf.peak.prop,function(x) x[1,1])
+times.u = sapply(apf.peak.prop,function(x) x[1,3])
+timesc = sapply(apf.peak.calc,function(x) x[1,2])
+timesc.l = sapply(apf.peak.calc,function(x) x[1,1])
+timesc.u = sapply(apf.peak.calc,function(x) x[1,3])
+ints = sapply(apf.peak.prop,function(x) x[2,2])
+ints.l = sapply(apf.peak.prop,function(x) x[2,1])
+ints.u = sapply(apf.peak.prop,function(x) x[2,3])
+intsc = sapply(apf.peak.calc,function(x) x[2,2])
+intsc.l = sapply(apf.peak.calc,function(x) x[2,1])
+intsc.u = sapply(apf.peak.calc,function(x) x[2,3])
+ymin = min(times.l,timesc.l,tpeak)
+ymax = max(times.u,timesc.u,tpeak)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Peaks-APF",param,"-",n,".pdf",sep=""),width=10,height=5)
+par(mfrow=c(1,2))
+plot(days,times,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Peak (Days)",main="Predicted Time of Epidemic Peak")
+points(days+0.5,timesc,pch=2)
+segments(days,times.l,days,times.u)
+segments(days+.5,timesc.l,days+.5,timesc.u)
+abline(h=tpeak,lty=2)
+legend("top",legend=c("Prop","Calc","95% Cred Int","Truth"),lty=c(NA,NA,1,2),
+	pch=c(1,2,NA,NA))
+ymin = min(ints.l,intsc.l,ipeak)
+ymax = max(ints.u,intsc.u,ipeak)
+plot(days,ints,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Proportion Infected",
+	main="Predicted Intensity of Epidemic Peak")
+points(days+0.5,intsc,pch=2)
+segments(days,ints.l,days,ints.u)
+segments(days+.5,intsc.l,days+.5,intsc.u)
+abline(h=ipeak,lty=2)
+dev.off()
+
+# Kernel density particle filter
+times = sapply(kd.peak.prop,function(x) x[1,2])
+times.l = sapply(kd.peak.prop,function(x) x[1,1])
+times.u = sapply(kd.peak.prop,function(x) x[1,3])
+timesc = sapply(kd.peak.calc,function(x) x[1,2])
+timesc.l = sapply(kd.peak.calc,function(x) x[1,1])
+timesc.u = sapply(kd.peak.calc,function(x) x[1,3])
+ints = sapply(kd.peak.prop,function(x) x[2,2])
+ints.l = sapply(kd.peak.prop,function(x) x[2,1])
+ints.u = sapply(kd.peak.prop,function(x) x[2,3])
+intsc = sapply(kd.peak.calc,function(x) x[2,2])
+intsc.l = sapply(kd.peak.calc,function(x) x[2,1])
+intsc.u = sapply(kd.peak.calc,function(x) x[2,3])
+ymin = min(times.l,timesc.l,tpeak)
+ymax = max(times.u,timesc.u,tpeak)
+pdf(paste("C:/Users/Danny/Dropbox/SIR_Particle_Filtering/Graphs/PF/Peaks-KD",param,"-",n,".pdf",sep=""),width=10,height=5)
+par(mfrow=c(1,2))
+plot(days,times,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Peak (Days)",main="Predicted Time of Epidemic Peak")
+points(days+0.5,timesc,pch=2)
+segments(days,times.l,days,times.u)
+segments(days+.5,timesc.l,days+.5,timesc.u)
+abline(h=tpeak,lty=2)
+legend("top",legend=c("Prop","Calc","95% Cred Int","Truth"),lty=c(NA,NA,1,2),
+	pch=c(1,2,NA,NA))
+ymin = min(ints.l,intsc.l,ipeak)
+ymax = max(ints.u,intsc.u,ipeak)
+plot(days,ints,ylim=c(ymin,ymax),xlim=c(min(days),max(days)+1),
+	xlab="Days",ylab="Proportion Infected",
+	main="Predicted Intensity of Epidemic Peak")
+points(days+0.5,intsc,pch=2)
+segments(days,ints.l,days,ints.u)
+segments(days+.5,intsc.l,days+.5,intsc.u)
+abline(h=ipeak,lty=2)
 dev.off()
