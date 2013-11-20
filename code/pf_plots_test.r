9836db958b5e11b5672738c402c6154a3d2a2202
dsheinson <sheinson@pstat.ucsb.edu>
2013-11-20 02:02:22 -0800

    Added functions to run kd and rm particle filters and plot quantiles of unknown parameter estimates

--- code/pf_plots_test.r
+++ code/pf_plots_test.r
@@ -8,20 +8,22 @@ dpath = "../data/"
 
 # Set graphical parameters
 params = expression(beta,gamma)
-ymins = c(0.15,0.085,0.9)
-ymaxs = c(0.35,0.145,1.35)
-cols = c(2,4,3)
+ymins = c(0.14,0.09,0.95)
+ymaxs = c(0.50,0.143,1.3)
+cols = c(3, 6)
 cex.lab = 6
 cex.main = 7
 cex.axis = 4
 cex.leg = 4
+pic.fac = 10
+burn = 0
 
 # length(ns) by length(params) figure of plot panels (rows = num particles, cols = params) with length(filts.load) credible bounds
 filts.load = c("KD","RM")
-ns = c(100)
+ns = c(100,1000,10000,20000)
 
 # Construct plots
-pdf(paste(gpath,"PF-systematic-uniform-",ns[length(ns)],".pdf",sep=""),width=10*length(params),height=10*length(ns))
+pdf(paste(gpath,"PF-systematic-lognormal-",ns[length(ns)],".pdf",sep=""),width=pic.fac*length(params),height=pic.fac*length(ns))
 par(mfrow=c(length(ns),length(params)),mar=c(9,11,7,1)+.1,mgp=c(7,2,0))
 for(i in 1:length(ns))
 {
@@ -29,30 +31,38 @@ for(i in 1:length(ns))
   {
     for(j in 1:length(filts.load))
     {
-      load(paste(dpath,"PF-quant-orig-orig-",filts.load[j],"-uniform-systematic-",ns[i],"-ess-80.rdata",sep=""))
+      load(paste(dpath,"PF-quant-orig-orig-",filts.load[j],"-lognormal-systematic-",ns[i],"-ess-80.rdata",sep=""))
       out = pf.quant.out$theta.quant
       tt = dim(out)[1]; nt = tt - 1
+      if(burn > 0)
+      {
+        quant = out[-burn,k,]
+        x = (0:nt)[-burn]
+      } else {
+        quant = out[,k,]
+        x = 0:nt
+      }
       if(j == 1) # call plot function
       {
         if(k == 1 & i == 1) # label y axis and title
         {
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab=paste("J = ",ns[i],sep=""),main=params[k],cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab=paste("J = ",ns[i],sep=""),main=params[k],cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         } else if(k == 1 & i == length(ns)) { # label x and y axes
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="Time (days)",ylab=paste("J = ",ns[i],sep=""),cex.lab=cex.lab,cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="Time (days)",ylab=paste("J = ",ns[i],sep=""),cex.lab=cex.lab,cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         } else if(k == 1) { # label y axis only
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab=paste("J = ",ns[i],sep=""),cex.lab=cex.lab,cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab=paste("J = ",ns[i],sep=""),cex.lab=cex.lab,cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         } else if(i == 1) { # label title only
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab="",main=params[k],cex.main=cex.main,cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab="",main=params[k],cex.main=cex.main,cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         } else if(i == length(ns)) { # label x axis only
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="Time (days)",ylab="",cex.lab=cex.lab,cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="Time (days)",ylab="",cex.lab=cex.lab,cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         } else { # label nothing
-          plot(1:nt,out[-1,k,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab="",cex.axis=cex.axis)
-          lines(1:nt,out[-1,k,3],col=cols[j])
+          plot(x,quant[,2],type="l",ylim=c(ymins[k],ymaxs[k]),col=cols[j],xlab="",ylab="",cex.axis=cex.axis)
+          lines(x,quant[,3],col=cols[j])
         }
       } else { # lines only
         lines(1:nt,out[-1,k,2],col=cols[j])
--- code/pf_plyr.r
+++ code/pf_plyr.r
@@ -1,19 +1,8 @@
 source("pf_plyr_functions.r")
 require(plyr)
 
-pf(n = 100, filt = "RM", resamp = "systematic", prior = "uniform", mod.data = "orig", mod.fit = "orig", seed = 61)
-pf.quant(n = 100, filt = "RM", resamp = "systematic", prior = "uniform", mod.data = "orig", mod.fit = "orig")
-
-mydata = expand.grid(n = c(100,1000,10000,20000), filt="KD", resamp="systematic", prior="uniform", mod.data="orig", mod.fit="orig", progress=FALSE, stringsAsFactors=FALSE)
+mydata = expand.grid(n = c(100,1000,10000,20000), filt=c("KD","RM"), resamp="systematic", prior="lognormal", mod.data="orig", mod.fit="orig", progress=TRUE, stringsAsFactors=FALSE)
 mydata = data.frame(mydata, seed = 61:64)
-
-# Apply pf to combination of pfs
-#data1 = expand.grid(mod.dat = "orig", mod.fit = "orig", n = c(100, 1000, 10000, 20000, 40000), filt = c("BF","APF","KD"), resamp = "systematic", prior = "uniform", progress=FALSE, stringsAsFactors=FALSE)
-#data2 = data.frame(mod.dat = "orig", mod.fit = "orig", n = c(10000, 20000), filt = "KD", resamp = "systematic", prior = "semi-uniform", progress=FALSE, stringsAsFactors=FALSE)
-#data3 = expand.grid(mod.dat = "orig", mod.fit = "orig", n = c(100, 1000, 10000, 20000, 40000), filt = "KD", resamp = c("multinomial","residual","stratified","systematic"), prior = "lognormal", progress=FALSE, stringsAsFactors=FALSE)
-#data4 = expand.grid(mod.dat = "ext", mod.fit = c("ext","orig"), n = c(100, 1000, 10000, 20000, 40000), filt = "KD", resamp = "stratified", prior = "lognormal", prior.samp = "lognormal", progress=FALSE, stringsAsFactors=FALSE)
-#seed = c(rep(61:65,3), 63, 64, rep(61:65, 4))
-#mydata = data.frame(rbind(data1,data2,data3),seed)
 
 require(doMC)
 registerDoMC()
