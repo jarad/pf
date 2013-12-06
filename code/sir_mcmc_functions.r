44de5004316b624ba575c9218a6a8f9cc3537214
dsheinson <sheinson@pstat.ucsb.edu>
2013-12-05 21:29:12 -0800

    Updated sir mcmc functions to have separate tuning parameters for joint states over time

--- code/sir_mcmc_functions.r
+++ code/sir_mcmc_functions.r
@@ -1,7 +1,16 @@
 source("sir_functions.r")
 
+# sir_mcmc - function to run MCMC for SIR model described in 'sir_functions.r'
+# Arguments:
+# y - L by T data matrix with L = number of data streams and T = number of time points. y may contain NA's for missing data.
+# psi - list contain known parameters P, b, varsigma, sigma, and eta
+# initial - optionally, a list specifying initial values of unknown states x (2 by T+1 matrix) parameters theta (length-3 vector)
+# tuning - optionally, a list containing initial values of tuning parameters tuning.x (2 by T+1 matrix) for sampling states and tuning.theta (length-3 vector) for sampling parameters
+# mcmc.details - optionally, a list containing scalars n.thin, n.sims, and n.burn for the how often to save iterations, total number of iterations, and burn in period, respectively, and boolean tune to specify whether tuning parameters should be adjusted during burn-in period
+# steps - optionally, a character vector specifying which states/unknown parameters to sample from in the Gibbs sampler. May contain any of 'x', 'beta', 'gamma', or 'nu'.
+# progress - boolean, should progress bar be displayed?
 sir_mcmc <- function(y, psi, initial, tuning, mcmc.details, steps, progress=TRUE) {
-  nt = dim(y)[2]
+  nt = dim(y)[2] # How many time points?
 
   # Deal with missing arguments
   if(missing(initial)) {
@@ -20,10 +29,8 @@ sir_mcmc <- function(y, psi, initial, tuning, mcmc.details, steps, progress=TRUE
   }
   if(missing(tuning))
   {
-    tuning.x = c(0.001, 0.001)
-    tuning.beta = 0.01
-    tuning.gamma = 0.001
-    tuning.nu = 0.01
+    tuning.x = matrix(0.0001, nr=2, nc = nt+1)
+    tuning.theta = c(0.001, 0.001, 0.01)
   } else {
     attach(tuning)
   }
@@ -45,40 +52,41 @@ sir_mcmc <- function(y, psi, initial, tuning, mcmc.details, steps, progress=TRUE
   keep.theta <- matrix(NA, n.iter, 3)
   accept.x <- matrix(NA, n.sims, nt + 1)
   accept.beta <- accept.gamma <- accept.nu <- rep(NA, n.sims)
-  tune.all <- matrix(NA, n.sims, 5)
+  tune.states.all <- array(NA, c(2, nt+1, n.sims))
+  tune.params.all <- matrix(NA, nr=n.sims, nc=3)
 
   # Run mcmc
   if(progress) pb = txtProgressBar(0,n.sims,style=3)
   for (i in 1:n.sims) {
     if (progress) setTxtProgressBar(pb,i)
-    if(i <= n.burn & tune) burn = TRUE else burn = FALSE
+    if(i <= n.burn & tune) tune.burn = TRUE else tune.burn = FALSE
     
     if('x'  %in% steps)
     {
-      samp.x = sample.x(y, x, theta, psi, tuning.x, burn)
+      samp.x = sample.x(y, x, theta, psi, tuning.x, tune.burn)
       x = samp.x$x
-      tuning.x <- tune.all[i,1:2] <- samp.x$tuning
+      tuning.x <- tune.states.all[,,i] <- samp.x$tuning
       accept.x[i,] = samp.x$accept
     }
     if('beta' %in% steps)
     {
-      samp.beta = sample.beta(y, x, theta, psi, tuning.beta, burn)
+      samp.beta = sample.beta(y, x, theta, psi, tuning.theta[1], tune.burn)
       theta[1] = samp.beta$beta
-      tuning.beta <- tune.all[i,3] <- samp.beta$tuning
+      tuning.theta[1] <- tune.params.all[i,1] <- samp.beta$tuning
       accept.beta[i] = samp.beta$accept
     }
     if('gamma' %in% steps)
     {
-      samp.gamma = sample.gamma(y, x, theta, psi, tuning.gamma, burn)
+      samp.gamma = sample.gamma(y, x, theta, psi, tuning.theta[2], tune.burn)
       theta[2] = samp.gamma$gamma
-      tuning.gamma <- tune.all[i,4] <- samp.gamma$tuning
+      tuning.theta[2] <- tune.params.all[i,2] <- samp.gamma$tuning
       accept.gamma[i] = samp.gamma$accept
     }
     if('nu' %in% steps)
     {
-      samp.nu = sample.nu(y, x, theta, psi, tuning.nu, burn)
+      samp.nu = sample.nu(y, x, theta, psi, tuning.theta[3], tune.burn)
       theta[3] = samp.nu$nu
-      tuning.nu <- tune.all[i,5] <- samp.nu$tuning
+      tuning.theta[3] <- tune.params.all[i,3] <- samp.nu$tuning
       accept.nu[i] = samp.nu$accept
     }
 
@@ -89,26 +97,27 @@ sir_mcmc <- function(y, psi, initial, tuning, mcmc.details, steps, progress=TRUE
     }
   }
 
-  return(list(x=keep.x, theta=keep.theta, tuning=tune.all, accept.x=accept.x, accept.theta=cbind(accept.beta,accept.gamma,accept.nu)))
+  return(list(x=keep.x, theta=keep.theta, tuning=list(states=tune.states.all,params=tune.params.all), accept.x=accept.x, accept.theta=cbind(accept.beta,accept.gamma,accept.nu)))
 }
 
-rmove <- function(y, x, theta, psi, tuning)
-{
-  nt = dim(y)[2]
-  stopifnot(nt == dim(x)[2] - 1)
+#rmove <- function(y, x, theta, psi, tuning)
+#{
+#  nt = dim(y)[2]
+#  stopifnot(nt == dim(x)[2] - 1)
+#
+#  # Move states
+#  x = sample.x(y, x, theta, psi, tuning[1:2])
+#  
+#  # Move parameters
+#  theta[1] = sample.beta(y, x, theta, psi, tuning[3])
+#  theta[2] = sample.gamma(y, x, theta, psi, tuning[4])
+#  theta[3] = sample.nu(y, x, theta, psi, tuning[5])
+#  
+#  return(list(state=x, theta=theta))
+#}
 
-  # Move states
-  x = sample.x(y, x, theta, psi, tuning[1:2])
-  
-  # Move parameters
-  theta[1] = sample.beta(y, x, theta, psi, tuning[3])
-  theta[2] = sample.gamma(y, x, theta, psi, tuning[4])
-  theta[3] = sample.nu(y, x, theta, psi, tuning[5])
-  
-  return(list(state=x, theta=theta))
-}
-
-sample.x <- function(y, x, theta, psi, tuning, burn = FALSE)
+# sample.x - function to sample from the full conditional of the state vector x at each time point separately, starting from initial state x[,0]
+sample.x <- function(y, x, theta, psi, tuning, tune = FALSE)
 {
   # Check dimensions
   nt = dim(y)[2]
@@ -116,48 +125,48 @@ sample.x <- function(y, x, theta, psi, tuning, burn = FALSE)
   accept = rep(FALSE, nt + 1)
   
   # resample prior state
-  i0 = rx(x[,1], tuning)[2]
+  i0 = rx(x[,1], tuning[,1])[2]
   x.prop = c(1-i0,i0)
   logMH = dlevo(x[,2], x.prop, theta, psi$P) + dlx0(x.prop) - dlevo(x[,2], x[,1], theta, psi$P) - dlx0(x[,1])
   if(log(runif(1)) < logMH)
   {
     x[,1] = x.prop
     accept[1] = TRUE
-    if(burn) tuning = tuning*1.1
+    if(tune) tuning[,1] = tuning[,1]*1.1
   } else {
-    if(burn) tuning = tuning / 1.1 
+    if(tune) tuning[,1] = tuning[,1] / 1.1 
   }
   
   # resample middle states
   for(i in 2:nt)
   {
-    x.prop = rx(x[,i], tuning)
+    x.prop = rx(x[,i], tuning[,i])
     logMH = dllik(y[,i-1], x.prop, psi$b, psi$varsigma, psi$sigma, psi$eta) + dlevo(x[,i+1], x.prop, theta, psi$P) + dlevo(x.prop, x[,i-1], theta, psi$P) - dllik(y[,i-1], x[,i], psi$b, psi$varsigma, psi$sigma, psi$eta) - dlevo(x[,i+1], x[,i], theta, psi$P) - dlevo(x[,i], x[,i-1], theta, psi$P)
     if(log(runif(1)) < logMH)
     {
       x[,i] = x.prop
       accept[i] = TRUE
-      if(burn) tuning = tuning*1.1
+      if(tune) tuning[,i] = tuning[,i]*1.1
     } else {
-      if(burn) tuning = tuning / 1.1 
+      if(tune) tuning[,i] = tuning[,i] / 1.1 
     }
   }
   
   # resample last state
-  x.prop = rx(x[,nt+1], tuning)
+  x.prop = rx(x[,nt+1], tuning[,nt+1])
   logMH = dllik(y[,nt], x.prop, psi$b, psi$varsigma, psi$sigma, psi$eta) + dlevo(x.prop, x[,nt], theta, psi$P) - dllik(y[,nt], x[,nt+1], psi$b, psi$varsigma, psi$sigma, psi$eta) - dlevo(x[,nt+1], x[,nt], theta, psi$P)
   if(log(runif(1)) < logMH)
   {
     x[,nt+1] = x.prop
     accept[nt + 1] = TRUE
-    if(burn) tuning = tuning*1.1
+    if(tune) tuning[,nt+1] = tuning[,nt+1]*1.1
   } else {
-    if(burn) tuning = tuning / 1.1
+    if(tune) tuning[,nt+1] = tuning[,nt+1] / 1.1
   }
   return(list(x=x,accept=accept,tuning=tuning))
 }
 
-sample.beta <- function(y, x, theta, psi, tuning, burn = FALSE)
+sample.beta <- function(y, x, theta, psi, tuning, tune = FALSE)
 {
   # Check dimensions
   nt = dim(y)[2]
@@ -180,15 +189,15 @@ sample.beta <- function(y, x, theta, psi, tuning, burn = FALSE)
   { 
     beta.curr = beta.prop
     accept = TRUE
-    if(burn) tuning = tuning*1.1
+    if(tune) tuning = tuning*1.1
   } else {
-    if(burn) tuning = tuning / 1.1
+    if(tune) tuning = tuning / 1.1
   }
   
   return(list(beta=beta.curr,accept=accept,tuning=tuning))
 }
 
-sample.gamma <- function(y, x, theta, psi, tuning, burn = FALSE)
+sample.gamma <- function(y, x, theta, psi, tuning, tune = FALSE)
 {
   # Check dimensions
   nt = dim(y)[2]
@@ -211,15 +220,15 @@ sample.gamma <- function(y, x, theta, psi, tuning, burn = FALSE)
   {
     gamma.curr = gamma.prop
     accept = TRUE
-    if(burn) tuning = tuning*1.1
+    if(tune) tuning = tuning*1.1
   } else {
-    if(burn) tuning = tuning / 1.1
+    if(tune) tuning = tuning / 1.1
   }
 
   return(list(gamma=gamma.curr,accept=accept,tuning=tuning))
 }
 
-sample.nu <- function(y, x, theta, psi, tuning, burn = FALSE)
+sample.nu <- function(y, x, theta, psi, tuning, tune = FALSE)
 {
   # Check dimensions
   nt = dim(y)[2]
@@ -242,9 +251,9 @@ sample.nu <- function(y, x, theta, psi, tuning, burn = FALSE)
   {
     nu.curr = nu.prop
     accept = TRUE
-    if(burn) tuning = tuning*1.1
+    if(tune) tuning = tuning*1.1
   } else {
-    if(burn) tuning = tuning / 1.1
+    if(tune) tuning = tuning / 1.1
   }
   
   return(list(nu=nu.curr,accept=accept,tuning=tuning))
