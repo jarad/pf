9c9e2dd5c1053eedce9bbc1252b4751d65ed5539
dsheinson <sheinson@pstat.ucsb.edu>
2014-01-27 13:57:57 -0800

    Updated scatterplots of beta v gamma

--- code/pf_plots.r
+++ code/pf_plots.r
@@ -294,7 +294,7 @@ for(k in 1:length(trans))
   load(paste(dpath,"PF-1-10000-KD-systematic-",prior[k],"-",trans[k],"-0.99-61.rdata",sep=""))
   
   # Resample particles at cutoff points to have equal weights
-  cutoff = seq(1, 61, len=5)
+  cutoff = seq(1, 61, len=4)
   betas = pf.out$ftheta(pf.out$out$theta[1,,],1)
   gammas = pf.out$ftheta(pf.out$out$theta[2,,],2)
   myout = array(NA,dim=c(2,dim(betas)[1],dim(betas)[2]))
