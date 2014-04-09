da023e355578a733fc140441055d817f5df3f963
dsheinson <sheinson@pstat.ucsb.edu>
2014-04-08 18:51:47 -0700

    Uploaded file to construct plots and compute 95% CI for pmcmc

--- code/sir_pmcmc_plots.r
+++ code/sir_pmcmc_plots.r
@@ -1,3 +1,5 @@
+require(pomp)
+
 # Set data and graphics path
 dpath = "../data/"
 gpath = "../graphs/"
@@ -11,43 +13,59 @@ varsigma = mysims[[1]]$true.params$varsigma
 sigma = mysims[[1]]$true.params$sigma
 eta = mysims[[1]]$true.params$eta
 
-# Load PMCMC objects
-n.chains = 3
-load(paste(dpath,"sir_pmcmc_test-1-1100-100.rdata",sep=""))
-niter = dim(conv.rec(out))[1] - 1
-nthin = 1
-nburn = 100
-out.beta = matrix(NA, nr = (niter - nburn) %/% nthin, nc=n.chains)
-out.gamma = matrix(NA, nr = (niter - nburn) %/% nthin, nc=n.chains)
-out.nu = matrix(NA, nr = (niter - nburn) %/% nthin, nc=n.chains)
-out.theta = list(); length(out.theta) = n.chains
-iter = seq(nburn+nthin+1,niter+1,nthin)
-for(i in 1:n.chains)
+# Function to create traceplots and tables with 95% credible intervals
+sir_pmcmc_plots <- function(n.chains, niter, np, nburn = 0, nthin = 1, hsd = FALSE)
 {
-  load(paste(dpath,"sir_pmcmc_test-",i,"-1100-100.rdata",sep=""))
-  out.theta[[i]] = conv.rec(out)[,4:6]
-  theta.temp = conv.rec(out)[iter,4:6]
-  out.beta[,i] = theta.temp[,1]
-  out.gamma[,i] = theta.temp[,2]
-  out.nu[,i] = theta.temp[,3]
+  # Load pmcmc objects
+  this.out = list(); length(this.out) = n.chains
+  if(hsd) hsd = "-hsd" else hsd = ""
+  for(i in 1:n.chains)
+  {
+    load(paste(dpath,"sir_pmcmc_test",hsd,"-",paste(i,niter,np,sep="-"),".rdata",sep=""))
+    this.out[[i]] = out
+    rm(out)
+  }
+  out = this.out
+  rm(this.out)
+  
+  # Create traceplots for fixed parameters
+  iter = seq(nburn+nthin+1,niter+1,nthin)
+  xlabs = c("","","Iteration")
+  ylabs = expression(beta,gamma,nu)
+  pdf(paste(gpath,"sir_pmcmc_test",hsd,"-",paste(i,niter,np,sep="-"),"-traceplots.pdf",sep=""))
+  par(mfrow=c(3,1),mar=c(5,7,4,2)+0.1)
+  for(i in 1:3)
+  {
+    ymin = min(sapply(out, function(x) conv.rec(x)[iter,3+i]))
+    ymax = max(sapply(out, function(x) conv.rec(x)[iter,3+i]))
+    plot(iter,conv.rec(out[[1]])[iter,3+i],type = "l",ylim=c(ymin,ymax),xlab=xlabs[i],ylab=ylabs[i],cex.lab=1.75)
+    if(n.chains > 1) 
+    {
+      for(j in 2:n.chains) lines(iter, conv.rec(out[[j]])[iter,3+i], col = 2*(j-1))
+    }
+  }
+  dev.off()
+  
+  # Plot filtered means of states
+  pdf(paste(gpath,"sir_pmcmc_test",hsd,"-",paste(i,niter,np,sep="-"),"-meanFilteredStates.pdf",sep=""))
+  par(mar=c(5,7,4,2)+0.1)
+  for(i in 1:3)
+  {
+    filt = filter.mean(out[[i]])
+    if(i == 1) plot(1:dim(filt)[2],filt[1,],type="l",ylim=c(0,1),xlab="Time",ylab="Filtered Mean",main="Filtered Means of s and i",cex.lab=1.75) else lines(1:dim(filt)[2],filt[1,], col = 2*(i-1))
+    if(i == 1) lines(1:dim(filt)[2],filt[2,]) else lines(1:dim(filt)[2],filt[2,],col=2*(i-1))
+  }
+  legend("topright",legend=1:n.chains,lty=c(1,1,1),col=c(1,2*(1:(n.chains-1))),cex=1.75)
+  dev.off()
+  
+  # Calculate 95% credible intervals
+  cred.int = matrix(NA, nr=2, nc = 3)
+  colnames(cred.int) = c("beta","gamma","nu")
+  for(i in 1:3) cred.int[,i] = quantile(sapply(out, function(x) conv.rec(x)[iter,3+i]),c(0.025,0.975))
+  print(cred.int)
 }
 
-# Compute 95% credible intervals
-cred.int = matrix(NA, nr=2, nc = 3)
-colnames(cred.int) = c("beta","gamma","nu")
-for(i in 1:3) cred.int[,i] = quantile(sapply(out.theta, function(x) x[,i]),c(0.025,0.975))
-print(cred.int)
-
-# Construct traceplots
-windows()
-par(mfrow=c(n.chains,1),mar=c(5,7,4,2)+0.1)
-plot(iter, out.beta[,1], type="l", ylim = c(min(out.beta),max(out.beta)), xlab="", ylab = expression(beta), cex.lab = 1.75)
-lines(iter, out.beta[,2], col = 2)
-lines(iter, out.beta[,3], col = 4)
-plot(iter, out.gamma[,1], type="l", ylim = c(min(out.gamma),max(out.gamma)), xlab="", ylab = expression(gamma), cex.lab = 1.75)
-lines(iter, out.gamma[,2], col = 2)
-lines(iter, out.gamma[,3], col = 4)
-plot(iter, out.nu[,1], type="l", ylim = c(min(out.nu),max(out.nu)), xlab="Iteration", ylab = expression(nu), cex.lab = 1.75)
-lines(iter, out.nu[,2], col = 2)
-lines(iter, out.nu[,3], col = 4)
-
+# Process pmcmc objects
+require(plyr)
+mydata = expand.grid(n.chains = 3, niter = 1100, np = 100, nburn = 100, nthin = 1, hsd = FALSE, stringsAsFactors = FALSE)
+m_ply(mydata, sir_pmcmc_plots)
