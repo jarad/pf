9c5082176304c9b04e912076503dcc185bf1b869
dsheinson <sheinson@pstat.ucsb.edu>
2014-04-10 15:04:52 -0700

    Fixed bug in pmcmc_test

--- code/sir_pmcmc_test.r
+++ code/sir_pmcmc_test.r
@@ -18,21 +18,22 @@ sir.pomp <- pomp(data = y, times = 1:dim(y)[2], t0 = 0, P=P, b=b, varsigma=varsi
 
 # Run pmcmc
 rw.sd = c(0.005, 0.001, 0.01); names(rw.sd) = c('beta','gamma','nu')
-sir_pmcmc <- function(n.chain, niter, np, ymax)
+sir_pmcmc <- function(n.chain, niter, np, y.max)
 {
   # Create pomp object
-  sir.pomp <- pomp(data = y[,1:ymax], times = 1:dim(y)[2], t0 = 0, P=P, b=b, varsigma=varsigma, sigma=sigma, eta = eta, rprocess=rprocess, rmeasure=rmeasure, dmeasure=dmeasure, skeleton = skeleton, skeleton.type = "map", initializer=initializer, rprior=rprior.pomp, dprior=dprior)
+  y = y[,1:y.max]
+  sir.pomp <- pomp(data = y, times = 1:dim(y)[2], t0 = 0, P=P, b=b, varsigma=varsigma, sigma=sigma, eta = eta, rprocess=rprocess, rmeasure=rmeasure, dmeasure=dmeasure, skeleton = skeleton, skeleton.type = "map", initializer=initializer, rprior=rprior.pomp, dprior=dprior)
   
   # Run pmcmc
   pars.init = mysims[[1]]$true.params$theta
   names(pars.init) = c('beta','gamma','nu')
   time = system.time(out <- pmcmc(sir.pomp, Nmcmc = niter, start = pars.init, pars = c('beta','gamma','nu'), rw.sd = rw.sd, Np = np, max.fail = Inf))
-  file = paste(dpath,"sir_pmcmc_test-hsd-",paste(n.chain,niter,np,ymax,sep="-"),".rdata",sep="")
+  file = paste(dpath,"sir_pmcmc_test-hsd-",paste(n.chain,niter,np,y.max,sep="-"),".rdata",sep="")
   save(out, file = file)
   print(time)
 }
 require(plyr)
 require(doMC)
 registerDoMC()
-mydata = expand.grid(n.chain=1:3, niter=20000, np=100, ymax = c(30,60,90,125))
+mydata = expand.grid(n.chain=1:3, niter=20000, np=100, y.max = c(30,60,90,125))
 m_ply(.data = mydata, .fun = sir_pmcmc, .parallel = TRUE)
