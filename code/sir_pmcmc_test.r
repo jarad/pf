cdf62b6e5aae010b226ca82bfc1251de5293ed4b
dsheinson <sheinson@pstat.ucsb.edu>
2014-04-11 11:21:43 -0700

    Code to run and plot pmcmc runs of 20000 iterations for data accumulated every 5th day

--- code/sir_pmcmc_test.r
+++ code/sir_pmcmc_test.r
@@ -13,9 +13,6 @@ varsigma = mysims[[1]]$true.params$varsigma
 sigma = mysims[[1]]$true.params$sigma
 eta = mysims[[1]]$true.params$eta
 
-# Create pomp object
-sir.pomp <- pomp(data = y, times = 1:dim(y)[2], t0 = 0, P=P, b=b, varsigma=varsigma, sigma=sigma, eta = eta, rprocess=rprocess, rmeasure=rmeasure, dmeasure=dmeasure, skeleton = skeleton, skeleton.type = "map", initializer=initializer, rprior=rprior.pomp, dprior=dprior)
-
 # Run pmcmc
 rw.sd = c(0.005, 0.001, 0.01); names(rw.sd) = c('beta','gamma','nu')
 sir_pmcmc <- function(n.chain, niter, np, y.max)
@@ -28,12 +25,12 @@ sir_pmcmc <- function(n.chain, niter, np, y.max)
   pars.init = mysims[[1]]$true.params$theta
   names(pars.init) = c('beta','gamma','nu')
   time = system.time(out <- pmcmc(sir.pomp, Nmcmc = niter, start = pars.init, pars = c('beta','gamma','nu'), rw.sd = rw.sd, Np = np, max.fail = Inf))
-  file = paste(dpath,"sir_pmcmc_test-hsd-",paste(n.chain,niter,np,y.max,sep="-"),".rdata",sep="")
+  file = paste(dpath,"sir_pmcmc_test-",paste(n.chain,niter,np,y.max,sep="-"),".rdata",sep="")
   save(out, file = file)
-  print(time)
+  cat(file,time[1:3],"\n")
 }
 require(plyr)
 require(doMC)
 registerDoMC()
-mydata = expand.grid(n.chain=1:3, niter=20000, np=100, y.max = c(30,60,90,125))
+mydata = expand.grid(n.chain=1:3, niter=20000, np=100, y.max = c(30,60,90,125,65,70,75,80,85,95,100,105,110,115,120,5,10,15,20,25,35,40,45,50,55))
 m_ply(.data = mydata, .fun = sir_pmcmc, .parallel = TRUE)
