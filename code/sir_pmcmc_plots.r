62a03f63a5b36080b7d5f599d0a7c45dd1e4b52c
dsheinson <sheinson@pstat.ucsb.edu>
2014-04-16 15:34:09 -0700

    Updated plots to compare multiple pmcmc runs with multiple kdpf runs

--- code/sir_pmcmc_plots.r
+++ code/sir_pmcmc_plots.r
@@ -16,13 +16,13 @@ sigma = mysims[[1]]$true.params$sigma
 eta = mysims[[1]]$true.params$eta
 
 # Function to create traceplots and tables with 95% credible intervals
-sir_pmcmc_plots <- function(n.chains, niter, np, y.max, nburn = 0, nthin = 1)
+sir_pmcmc_plots <- function(chains, niter, np, y.max, nburn = 0, nthin = 1)
 {
   # Load pmcmc objects
-  this.out = list(); length(this.out) = n.chains
-  for(i in 1:n.chains)
+  this.out = list(); length(this.out) = length(chains)
+  for(i in 1:length(chains))
   {
-    load(paste(dpath,"sir_pmcmc_test-",paste(i,niter,np,y.max,sep="-"),".rdata",sep=""))
+    load(paste(dpath,"sir_pmcmc_test-",paste(chains[i],niter,np,y.max,sep="-"),".rdata",sep=""))
     this.out[[i]] = out
     rm(out)
   }
@@ -33,7 +33,7 @@ sir_pmcmc_plots <- function(n.chains, niter, np, y.max, nburn = 0, nthin = 1)
   iter = seq(nburn+nthin+1,niter+1,nthin)
   xlabs = c("","","Iteration")
   ylabs = expression(beta,gamma,nu)
-  pdf(paste(gpath,"sir_pmcmc_test-",paste(i,niter,np,y.max,sep="-"),"-traceplots.pdf",sep=""))
+  pdf(paste(gpath,"sir_pmcmc_test-",paste(chains,collapse="-"),"-",paste(length(chains),niter,np,y.max,sep="-"),"-traceplots.pdf",sep=""))
   par(mfrow=c(3,1),mar=c(5,7,4,2)+0.1)
   for(i in 1:3)
   {
@@ -42,9 +42,9 @@ sir_pmcmc_plots <- function(n.chains, niter, np, y.max, nburn = 0, nthin = 1)
     param = conv.rec(out[[1]])[iter,3+i]
     param.all = param
     plot(iter,param,type = "l",ylim=c(ymin,ymax),xlab=xlabs[i],ylab=ylabs[i],cex.lab=1.75)
-    if(n.chains > 1) 
+    if(length(chains) > 1) 
     {
-      for(j in 2:n.chains)
+      for(j in 2:length(chains))
       {
         param = conv.rec(out[[j]])[iter,3+i]
         param.all = c(param.all,param)
@@ -56,15 +56,15 @@ sir_pmcmc_plots <- function(n.chains, niter, np, y.max, nburn = 0, nthin = 1)
   dev.off()
   
   # Plot filtered means of states
-  pdf(paste(gpath,"sir_pmcmc_test-",paste(i,niter,np,y.max,sep="-"),"-meanFilteredStates.pdf",sep=""))
+  pdf(paste(gpath,"sir_pmcmc_test-",paste(chains,collapse="-"),"-",paste(length(chains),niter,np,y.max,sep="-"),"-meanFilteredStates.pdf",sep=""))
   par(mar=c(5,7,4,2)+0.1)
-  for(i in 1:3)
+  for(i in 1:length(chains))
   {
     filt = filter.mean(out[[i]])
     if(i == 1) plot(1:dim(filt)[2],filt[1,],type="l",ylim=c(0,1),xlab="Time",ylab="Filtered Mean",main="Filtered Means of s and i",cex.lab=1.75) else lines(1:dim(filt)[2],filt[1,], col = 2*(i-1))
     if(i == 1) lines(1:dim(filt)[2],filt[2,]) else lines(1:dim(filt)[2],filt[2,],col=2*(i-1))
   }
-  legend("topright",legend=1:n.chains,lty=c(1,1,1),col=c(1,2*(1:(n.chains-1))),cex=1.75)
+  legend("topright",legend=chains,lty=rep(1,length(chains)),col=c(1,2*(1:(length(chains)-1))),cex=1.75)
   dev.off()
   
   # Calculate 95% credible intervals
@@ -77,9 +77,15 @@ sir_pmcmc_plots <- function(n.chains, niter, np, y.max, nburn = 0, nthin = 1)
 
 # Process pmcmc objects
 require(plyr)
-y.max = seq(5,125,5)
-mydata = expand.grid(n.chains = 3, niter = 20000, np = 100, nburn = 0, nthin = 1, y.max = y.max, stringsAsFactors = FALSE)
+y.max.pmcmc = c(30,60,65,70,75,80,85,90,125)
+n.iter = 30000
+mydata = expand.grid(chains = 1:3, niter = n.iter, np = 100, y.max = y.max.pmcmc, nburn = 0, nthin = 1, stringsAsFactors = FALSE)
 cred.int.pmcmc = maply(mydata, sir_pmcmc_plots)
+n.pmcmc = dim(cred.int.pmcmc)[1]
+# mydata = expand.grid(niter = n.iter, np = 100, y.max = y.max.pmcmc, nburn = 0, nthin = 1, stringsAsFactors = FALSE)
+# cred.int.pmcmc = maply(mydata, function(niter, np, y.max, nburn, nthin) sir_pmcmc_plots(1:3, niter, np, y.max, nburn, nthin))
+# cred.int.pmcmc = array(cred.int.pmcmc, c(1,dim(cred.int.pmcmc)))
+# n.pmcmc = 1
 
 # # Calculate ESS for KDPF runs
 # ess_kdpf <- function(y.max,n.sims,np)
@@ -127,12 +133,12 @@ params=expression(beta,gamma,nu)
 xlabs = c("Time","","")
 ylabs = c("Parameter Value","","")
 nt = dim(mysims[[1]]$sim$y)[2]
-pdf(file=paste(gpath,"sir-pmcmc-kdpf.pdf",sep=""),width=30,height=10)
+pdf(file=paste(gpath,"sir-pmcmc-kdpf-",n.pmcmc,"-",n.iter,"-",n.sims,"-",np,".pdf",sep=""),width=30,height=10)
 par(mfrow=c(1,3),mar=c(9,11,7,1)+.1,mgp=c(7,2,0))
 for(i in 1:3)
 {
-  ymin = min(cred.int.pmcmc[,1,i],ymin.kdpf[i],mysims[[1]]$true.params$theta[i])
-  ymax = max(cred.int.pmcmc[,2,i],ymax.kdpf[i],mysims[[1]]$true.params$theta[i])
+  ymin = min(cred.int.pmcmc[,,1,i],ymin.kdpf[i],mysims[[1]]$true.params$theta[i])
+  ymax = max(cred.int.pmcmc[,,2,i],ymax.kdpf[i],mysims[[1]]$true.params$theta[i])
   load(paste(dpath,"PF-quant-1-",np,"-KD-stratified-orig-log-0.99-",61,".rdata",sep=""))
   cred.int.kdpf = maply(.data = data.frame(y.max = y.max), .fun = kd_quant)
   plot(y.max,cred.int.kdpf[,1,i],type="b",ylim=c(ymin,ymax),lwd=2,col=3,xlab=xlabs[i],ylab=ylabs[i],main=params[i],cex.lab=6,cex.main=7,cex.axis=4,cex=2)
@@ -148,8 +154,11 @@ for(i in 1:3)
       lines(y.max,cred.int.kdpf[,2,i],type="b",col=3,lwd=2,cex=2)
     }
   }
-  points(y.max,cred.int.pmcmc[,1,i],lwd=4,col=6,cex=4)
-  points(y.max,cred.int.pmcmc[,2,i],lwd=4,col=6,cex=4)
-  if(i == 1) legend("bottomright",legend=c("KDPF","PMCMC"),lty=c(1,NA),pch=c(1,1),col=c(3,6),cex=4,pt.cex=c(2,4),lwd=c(2,4),bg="white",pt.bg="white")
+  for(k in 1:n.pmcmc)
+  {
+    points(y.max.pmcmc,cred.int.pmcmc[k,,1,i],lwd=4,col=2*(k-1)+1*(k==1),cex=4)
+    points(y.max.pmcmc,cred.int.pmcmc[k,,2,i],lwd=4,col=2*(k-1)+1*(k==1),cex=4)
+    if(i == 1) legend("bottomright",legend=c("KDPF","PMCMC"),lty=c(1,NA),pch=c(1,1),col=c(3,1),cex=4,pt.cex=c(2,4),lwd=c(2,4),bg="white",pt.bg="white")
+  }
 }
 dev.off()
